5153d0932b4f41fed9d922afbb3ac7e1d250ffd7
Faustina Fechita <faustina.fechita@gmail.com>
2015-09-15 17:00:34 +0100

    Protractor & InboxControllerSpecs

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -1,19 +1,40 @@
+//exports.config = {
+//  allScriptsTimeout: 11000,
+//
+//  specs: [
+//    '*.js'
+//  ],
+//
+//  capabilities: {
+//    'browserName': 'chrome'
+//  },
+//
+//  baseUrl: 'http://localhost:8000/app/',
+//
+//  framework: 'jasmine',
+//
+//  jasmineNodeOpts: {
+//    defaultTimeoutInterval: 30000
+//  }
+//};
+
+
 exports.config = {
-  allScriptsTimeout: 11000,
-
-  specs: [
-    '*.js'
-  ],
-
-  capabilities: {
-    'browserName': 'chrome'
-  },
-
-  baseUrl: 'http://localhost:8000/app/',
-
-  framework: 'jasmine',
-
+  specs: ['specs/*.js'],
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    showColors: true,
+    silent: true
+  },
+  baseUrl: 'http://localhost:8000/app',
+
+  onPrepare: function () {
+    var specReporter = require('jasmine-spec-reporter');
+    jasmine.getEnv().addReporter(new specReporter({ displayStacktrace: true }));
+
+    require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new jasmine.JUnitXmlReporter('results/', true, true));
+
+    var htmlReporter = require('protractor-html-screenshot-reporter');
+    jasmine.getEnv().addReporter(new htmlReporter({baseDirectory: 'screenshots'}));
   }
-};
+}
