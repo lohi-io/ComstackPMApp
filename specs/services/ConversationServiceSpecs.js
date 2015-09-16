71038cbae29a4db4a2588843721d2e55a89b33df
David Hughes <david.hughes125@gmail.com>
2015-09-16 17:30:42 +0100

    Linting: ConversationServiceSpecs.js

--- specs/services/ConversationServiceSpecs.js
+++ specs/services/ConversationServiceSpecs.js
@@ -1,38 +1,41 @@
-/**
- * Created by fechit01 on 15/09/2015.
- */
-(function (describe, it, expect, inject, angular, beforeEach, afterEach, spyOn, module) {
+/* globals describe, it, expect, inject, beforeEach */
+(function (describe, it, expect, inject, angular, beforeEach) {
+  describe('ConversationService', function() {
+    var mockBackend, loader;
 
-    describe('ConversationService', function() {
+    beforeEach(angular.mock.module('ComstackPmApp.Services', function ($provide) {
+      $provide.constant('ApiUrl', 'https://cancerchat01dev.prod.acquia-sites.com/api/v1');
+      $provide.constant('AccessToken', 'qNlIfE4RskDFnAin9ycg1NipeSnCtqWLLLzqVXBJ6dc');
+    }));
 
-        beforeEach(angular.mock.module("ComstackPmApp.Services", function ($provide) {
-            $provide.constant("ApiUrl", "https://cancerchat01dev.prod.acquia-sites.com/api/v1");
-            $provide.constant("AccessToken", "qNlIfE4RskDFnAin9ycg1NipeSnCtqWLLLzqVXBJ6dc");
-        }));
+    beforeEach(inject(function($injector){
+      mockBackend = $injector.get('$httpBackend');
+      loader = $injector.get('GetConversations');
+    }));
 
+    it('Should load conversation for current user', function() {
+        var response = {
+          'data': {
+            'user': {
+              'type': 'user',
+              'id': 66531,
+              'name': 'basic_user_1'
+            }
+          }
+        };
+        var result;
 
-        var mockBackend, user, loader, resource;
-        beforeEach(inject(function($injector)
-        {
-            mockBackend = $injector.get('$httpBackend');
-            resource = $injector.get('Conversation');
-            loader = $injector.get('GetConversations');
-        }));
-
-
-        it('Should load conversation for current user', function()
-        {
-            var response = {"data":{"user":{"type":"user","id":66531,"name":"basic_user_1"}}};
-            var result;
-
-            mockBackend.expectGET('https://cancerchat01dev.prod.acquia-sites.com/api/v1/cs-pm/conversations?access_token=qNlIfE4RskDFnAin9ycg1NipeSnCtqWLLLzqVXBJ6dc').respond(response);
-            var promise = loader();
-            promise.then(function(rec) { result = rec; });
-
-            expect(result).toBeUndefined();
-            mockBackend.flush();
-            expect(angular.equals(result, response)).toBeTruthy();
+        /* eslint-disable max-len */
+        mockBackend.expectGET('https://cancerchat01dev.prod.acquia-sites.com/api/v1/cs-pm/conversations?access_token=qNlIfE4RskDFnAin9ycg1NipeSnCtqWLLLzqVXBJ6dc').respond(response);
+        /* eslint-enable max-len */
+        var promise = loader();
+        promise.then(function(rec) {
+          result = rec;
         });
-    });
 
-})(describe, it, expect, inject, angular, beforeEach, afterEach, spyOn, angular.mock.module);
+        expect(result).toBeUndefined();
+        mockBackend.flush();
+        expect(angular.equals(result, response)).toBeTruthy();
+      });
+  });
+})(describe, it, expect, inject, angular, beforeEach);
