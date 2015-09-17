08a8486ac3bec14b43a934a3e5dc81cd4d8c7669
David Hughes <david.hughes125@gmail.com>
2015-09-17 09:43:36 +0100

    Linting: InboxController.js
    Added `app` to global variables acknowledged by eslint.

--- app/js/controllers/InboxController.js
+++ app/js/controllers/InboxController.js
@@ -1,30 +1,27 @@
-/**
- * Created by fechit01 on 11/09/2015.
- */
 app.controller('InboxCtrl', ['$scope', 'currentUser', 'conversations', '$state', '$stateParams',
-    function($scope, currentUser, conversations, $state, $stateParams)
-    {
-        $scope.currentPage = $stateParams.page;
-        $scope.currentUser = currentUser;
-        $scope.conversations = conversations.data;
-        $scope.conversationsCount = conversations.count;
+  function($scope, currentUser, conversations, $state, $stateParams) {
+    $scope.currentPage = $stateParams.page;
+    $scope.currentUser = currentUser;
+    $scope.conversations = conversations.data;
+    $scope.conversationsCount = conversations.count;
 
-        $scope.goToPage = function(page){
-            $state.go('inbox', {page: page})
-        };
+    $scope.goToPage = function(page) {
+      $state.go('inbox', {
+        page: page
+      });
+    };
 
-        $scope.previous = function(){
-            if(angular.isDefined($scope.conversations.previous)){
-                $scope.goToPage($scope.currentPage - 1);
-            }
-        };
+    $scope.previous = function() {
+      if (angular.isDefined($scope.conversations.previous)) {
+        $scope.goToPage($scope.currentPage - 1);
+      }
+    };
 
-        $scope.next = function(){
-            if(angular.isDefined($scope.conversations.next)){
-                $scope.goToPage($scope.currentPage + 1);
-            }
-        };
-
-    }
+    $scope.next = function() {
+      if (angular.isDefined($scope.conversations.next)) {
+        $scope.goToPage($scope.currentPage + 1);
+      }
+    };
+  }
 ]);
 
