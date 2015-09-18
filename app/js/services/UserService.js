f0310606b59e17f93e97026b21967b350b97c9eb
David Hughes <david.hughes125@gmail.com>
2015-09-18 11:47:26 +0100

    Linting: UserService.js

--- app/js/services/UserService.js
+++ app/js/services/UserService.js
@@ -3,32 +3,27 @@
  */
 var services = angular.module('ComstackPmApp.Services');
 
-services.factory('User', [
-  '$resource', 'ApiUrl', 'AccessToken',
-  function($resource,ApiUrl,AccessToken)
-  {
-    return $resource('',{}
-      ,{
-        getCurrentUser: {
-          method: 'GET',
-          url: ApiUrl +'/cs-pm/users/current-user?access_token='+AccessToken,
-          isArray: false
-        }
+services.factory('User', ['$resource', 'ApiUrl', 'AccessToken',
+  function($resource, ApiUrl, AccessToken) {
+    return $resource('', {}, {
+      getCurrentUser: {
+        method: 'GET',
+        url: ApiUrl + '/cs-pm/users/current-user?access_token=' + AccessToken,
+        isArray: false
       }
-    );
+    });
   }
 ]);
 
-services.factory('GetCurrentUser', [
-  'User', '$q', function(User, $q)
-  {
-    return function()
-    {
-      var delay = $q.defer();
-      User.getCurrentUser(
-        function (user) { delay.resolve(user); },
-        function() { delay.reject("Unable to fetch current user"); });
-        return delay.promise;
-    };
-  }
-]);
+services.factory('GetCurrentUser', ['User', '$q', function(User, $q) {
+  return function() {
+    var delay = $q.defer();
+    User.getCurrentUser(function (user) {
+        delay.resolve(user);
+      }, function() {
+        delay.reject('Unable to fetch current user');
+      }
+    );
+    return delay.promise;
+  };
+}]);
