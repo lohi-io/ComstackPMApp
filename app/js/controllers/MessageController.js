fc1095d0b2cde88b1f3391ce9b023f96b335128e
Faustina Fechita <faustina.fechita@gmail.com>
2015-10-01 12:19:54 +0100

    Conversation save with csrf token example_index

--- app/js/controllers/MessageController.js
+++ app/js/controllers/MessageController.js
@@ -18,44 +18,15 @@ app.controller('MessageCtrl', ['$scope', '$state', 'getAvailableUsers', 'configu
 
     $scope.new_conversation_header = config.getString('form__new_conversation__header', {user_id: ''});
 
-    //var savePost = function(data)
-    //{
-    //  var deferred = $q.defer();
-    //  console.log("Starting basic auth...");
-    //  $http.defaults.useXDomain = true;
-    //  $http.defaults.withCredentials = true;
-    //  $http.post('https://cancerchat01dev.prod.acquia-sites.com/api/v1/cs-pm/conversations', data,
-    //    { headers: { 'Content-Type': 'application/x-www-form-urlencoded',
-    //                 'X-CSRF-Token': 'bRv9z7xHezuHU0d_s7A7VCslg3Ek9t5nncZFpKGwKkA'
-    //                 }
-    //    }).success(function (response)
-    //  {
-    //    deferred.resolve(response);
-    //  }).error(function(err, status)
-    //  {
-    //    deferred.reject(err);
-    //  });
-    //
-    //  return deferred.promise;
-    //};
-
-
     $scope.save = function () {
 
       $scope.message.recipients = $scope.users.map(function (item) {
         return item.id
       });
 
-      //savePost($scope.message).then(function(response){
-      //  console.log(response);
-      //}, function(error){
-      //  console.log(error);
-      //});
-
-
-     // $http.defaults.headers.post['X-CSRF-Token']= 'aoEEbSiYSyFumm70UGc-lHCpdNAvr71jPq5g1vKe-QQ';
-      $scope.message.$save(function () {
-        $state.go('conversation', {}, {
+      $scope.message.$save(function (response) {
+        $state.go('conversation', {id: response.data.id},
+          {
           reload: 'conversation',
           inherit: false,
           notify: true
