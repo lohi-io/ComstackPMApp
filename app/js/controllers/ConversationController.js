e22b4fb845f7408625066243b629a70696a309da
David Hughes <david.hughes125@gmail.com>
2015-10-01 11:13:01 +0100

    View Conversation: Make 'Message' header dynamic.

--- app/js/controllers/ConversationController.js
+++ app/js/controllers/ConversationController.js
@@ -41,6 +41,7 @@ app.controller('ConversationCtrl', ['$scope', '$window', '$state', '$stateParams
       $scope.link__delete = config.getString('link__delete', {});
       $scope.link__report = config.getString('link__report', {});
       $scope.link__block = config.getString('link__block', {});
+      $scope.heading__messages = config.getString('heading__messages', {});
     };
 
     $scope.messages = {};
