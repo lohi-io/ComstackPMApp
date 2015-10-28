a318061c81347d567b0de3b41f3fab446b0b2be1
David Hughes <david.hughes125@gmail.com>
2015-10-28 10:49:30 +0000

    Add SquishFilter to Grunt uglify task.

--- gruntfile.js
+++ gruntfile.js
@@ -44,6 +44,7 @@ module.exports = function(grunt){
               'app/js/providers/ConfigurationServiceProvider.js',
               'app/js/directives/01_bootstrap.js',
               'app/js/directives/EmojiDirective.js',
+              'app/js/filters/SquishFilter.js',
               'app/js/filters/HtmlSafeFilter.js',
               'app/js/filters/TruncateFilter.js',
               'app/js/filters/DateFromNowFilter.js',
